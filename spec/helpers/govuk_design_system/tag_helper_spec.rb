b4e0dc7e0b080fd48d65ef0c65cfe0ec5d1f4a69
Alan Reid <alan.reid@businessandtrade.gov.uk>
2025-08-12 15:47:36 +0100

    Update gems, and fix broken specs

--- spec/helpers/govuk_design_system/tag_helper_spec.rb
+++ spec/helpers/govuk_design_system/tag_helper_spec.rb
@@ -3,9 +3,9 @@ require "rails_helper"
 RSpec.describe GovukDesignSystem::TagHelper, type: :helper do
   describe "#govukTag" do
     it "returns the correct HTML for the default example" do
-      html = helper.govukTag({
+      html = helper.govukTag(
         text: "completed"
-      })
+      )
 
       expect(html).to match_html(<<~HTML)
         <strong class="govuk-tag">
